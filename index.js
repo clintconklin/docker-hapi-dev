97c743eafe397c0cad8834213fff39eb4ff97f89
Clint Conklin <clint.conklin@gmail.com>
2017-06-29 07:26:36 -0700

    troubleshooting

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ server.route({
     'method': 'GET',
     'path': '/',
     'handler': function (request, reply) {
+		console.log('request.info.host -> ' + request.info.host);
         reply('<h1>This Is A Reverse Proxy Situation</h1><p>Greetings. This page is served from node via a reverse-proxied apache.</p>');
     }
 });
